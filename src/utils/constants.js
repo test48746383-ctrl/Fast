702b725e33993f0eb72dd92ba901f3163313e521
test48746383-ctrl <test48746383@gmail.com>
2026-02-01 23:30:09 +0530

    Create constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,36 @@
+export const APP_NAME = "AI Palm Reader";
+export const APP_VERSION = "1.0.0";
+
+export const ZODIAC_SIGNS = [
+  'aries', 'taurus', 'gemini', 'cancer', 'leo', 'virgo', 
+  'libra', 'scorpio', 'sagittarius', 'capricorn', 'aquarius', 'pisces'
+];
+
+export const PALM_LINES = {
+  HEART: 'heart_line',
+  HEAD: 'head_line',
+  LIFE: 'life_line',
+  FATE: 'fate_line'
+};
+
+export const TAROT_CATEGORIES = {
+  DAILY: 'daily',
+  LOVE: 'love',
+  CAREER: 'career',
+  GROWTH: 'growth'
+};
+
+export const GENDERS = {
+  MALE: 'male',
+  FEMALE: 'female',
+  OTHER: 'other'
+};
+
+export const STORAGE_KEYS = {
+  LANGUAGE: 'app_language',
+  USER_PROFILE: 'user_profile',
+  ONBOARDING: 'has_completed_onboarding',
+  CHAT_HISTORY: 'chat_history',
+  PALM_RESULT: 'palm_scan_result',
+  DAILY_READING: 'daily_reading_cache'
+};
