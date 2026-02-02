1df3ac801a65499d0738110b91766bd2d573ab05
test48746383-ctrl <test48746383@gmail.com>
2026-02-02 09:46:35 +0530

    Create Snackbar.jsx

---
+++ src/components/common/Snackbar.jsx
@@ -0,0 +1,4 @@
+// This is redundant with AppContext Toast but kept for index completeness
+// Will simply return null as toast is handled globally
+const Snackbar = () => null;
+export default Snackbar;
