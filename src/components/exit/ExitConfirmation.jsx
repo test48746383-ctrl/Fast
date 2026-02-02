12942c00cf6a1d5034af150a8f38a1fbbdb497de
test48746383-ctrl <test48746383@gmail.com>
2026-02-02 11:13:15 +0530

    Create ExitConfirmation.jsx

---
+++ src/components/exit/ExitConfirmation.jsx
@@ -0,0 +1,4 @@
+// Logic handled by useBackHandler hook and Toast
+// This component is kept for completeness if we want a custom Modal Exit later
+const ExitConfirmation = () => null;
+export default ExitConfirmation;
