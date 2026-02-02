95a63a0802d9777a495f973053206ea73f821085
test48746383-ctrl <test48746383@gmail.com>
2026-02-02 08:18:44 +0530

    Create zodiacData.js

---
+++ src/data/zodiacData.js
@@ -0,0 +1,104 @@
+/**
+ * ♈ Zodiac Data
+ * 
+ * Static data for Zodiac signs, icons, and date ranges.
+ */
+
+export const ZODIAC_SIGNS = [
+  {
+    id: 'aries',
+    name: 'Aries',
+    symbol: '♈',
+    dateRange: 'Mar 21 - Apr 19',
+    element: 'Fire',
+    planet: 'Mars'
+  },
+  {
+    id: 'taurus',
+    name: 'Taurus',
+    symbol: '♉',
+    dateRange: 'Apr 20 - May 20',
+    element: 'Earth',
+    planet: 'Venus'
+  },
+  {
+    id: 'gemini',
+    name: 'Gemini',
+    symbol: '♊',
+    dateRange: 'May 21 - Jun 20',
+    element: 'Air',
+    planet: 'Mercury'
+  },
+  {
+    id: 'cancer',
+    name: 'Cancer',
+    symbol: '♋',
+    dateRange: 'Jun 21 - Jul 22',
+    element: 'Water',
+    planet: 'Moon'
+  },
+  {
+    id: 'leo',
+    name: 'Leo',
+    symbol: '♌',
+    dateRange: 'Jul 23 - Aug 22',
+    element: 'Fire',
+    planet: 'Sun'
+  },
+  {
+    id: 'virgo',
+    name: 'Virgo',
+    symbol: '♍',
+    dateRange: 'Aug 23 - Sep 22',
+    element: 'Earth',
+    planet: 'Mercury'
+  },
+  {
+    id: 'libra',
+    name: 'Libra',
+    symbol: '♎',
+    dateRange: 'Sep 23 - Oct 22',
+    element: 'Air',
+    planet: 'Venus'
+  },
+  {
+    id: 'scorpio',
+    name: 'Scorpio',
+    symbol: '♏',
+    dateRange: 'Oct 23 - Nov 21',
+    element: 'Water',
+    planet: 'Pluto'
+  },
+  {
+    id: 'sagittarius',
+    name: 'Sagittarius',
+    symbol: '♐',
+    dateRange: 'Nov 22 - Dec 21',
+    element: 'Fire',
+    planet: 'Jupiter'
+  },
+  {
+    id: 'capricorn',
+    name: 'Capricorn',
+    symbol: '♑',
+    dateRange: 'Dec 22 - Jan 19',
+    element: 'Earth',
+    planet: 'Saturn'
+  },
+  {
+    id: 'aquarius',
+    name: 'Aquarius',
+    symbol: '♒',
+    dateRange: 'Jan 20 - Feb 18',
+    element: 'Air',
+    planet: 'Uranus'
+  },
+  {
+    id: 'pisces',
+    name: 'Pisces',
+    symbol: '♓',
+    dateRange: 'Feb 19 - Mar 20',
+    element: 'Water',
+    planet: 'Neptune'
+  }
+];
